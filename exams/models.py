1899023bb9d64a34f0acb8f5480014de86b1822c
myciek <maciejmycielski@gmail.com>
2019-05-25 14:58:49 +0200

    basic authentication

--- exams/models.py
+++ exams/models.py
@@ -1,1 +1,9 @@
+from django.db import models
 
+
+# Create your models here.
+
+
+class Example(models.Model):
+    text = models.CharField(max_length=200)
+    owner = models.ForeignKey('auth.User', related_name='examples', on_delete=models.CASCADE)
--- exams/serializers.py
+++ exams/serializers.py
@@ -1,14 +1,25 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from exams.models import Example
 
 
 class UserSerializer(serializers.ModelSerializer):
+    examples = serializers.PrimaryKeyRelatedField(many=True, queryset=Example.objects.all())
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'groups')
+        fields = ('id', 'username', 'email', 'examples')
 
 
-class GroupSerializer(serializers.ModelSerializer):
+class SignUpSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Group
-        fields = ('id', 'name')
+        model = User
+        fields = ('username', 'email', 'password')
+
+
+class ExampleSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Example
+        fields = ('text','owner')
